01fe1d4fa46e79ab7956e7e6c2d687fb8dade4e8
HoangT1215 <HoangT1215@gmail.com>
2017-04-17 10:43:46 -0700

    connected to the API

--- getdata.py
+++ getdata.py
@@ -1,30 +1,18 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import urllib2
-import requests as r
+import requests
+import json
 
 #send request GET to opendota
+def herodat():
+	r = requests.get('https://api.opendota.com/api/heroStats')
+	if r.status_code == 200:
+	    data = r.json()
+	df = pd.io.json.json_normalize(data)
+	print df.columns.values.tolist()
+	print df['name']
 
-a = [{"id":0
-	,"name":"string"
-	,"localized_name":"string"
-	,"img":"string"
-	,"icon":"string"
-	,"pro_win":0
-	,"pro_pick":0
-	,"hero_id":0
-	,"pro_ban":0
-	,"1000_pick":0
-	,"1000_win":0
-	,"2000_pick":0
-	,"2000_win":0
-	,"3000_pick":0
-	,"3000_win":0,
-	"4000_pick":0,
-	"4000_win":0,
-	"5000_pick":0,
-	"5000_win":0}]
-p = urllib2.urlopen("https://api.opendota.com/api/heroStats").read()
-data = pd.io.json.json_normalize(p)
-print data
+herodat()
+
 
---
+++ visualization.py
@@ -0,0 +1,8 @@
+#This is for playing with seaborn API
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#we can try visualizing stock data
